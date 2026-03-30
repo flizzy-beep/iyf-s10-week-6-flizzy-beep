3947dfc09ed2044d838416cac42dc5db264cb23b
flizzy-beep <christianosilali8@gmail.com>
2026-03-30 21:30:35 +0000

    Add: Task 11.1 async understanding

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Task 11.1 — Understanding Async
+
 console.log("A");
 
 setTimeout(() => console.log("B"), 0);
@@ -7,3 +9,25 @@ console.log("C");
 setTimeout(() => console.log("D"), 100);
 
 console.log("E");
+
+// Callback example
+
+function loadUser(userId, callback) {
+
+    setTimeout(() => {
+
+        const user = {
+            id: userId,
+            name: "User " + userId,
+            age: 25
+        };
+
+        callback(user);
+
+    }, 1500);
+
+}
+
+loadUser(1, function(user){
+    console.log("Loaded user:", user);
+});
