2d9e199b6b87280472415916221d5094eb1b5825
flizzy-beep <christianosilali8@gmail.com>
2026-03-31 19:16:03 +0000

    Fetch single user using async await

--- app.js
+++ app.js
@@ -222,3 +222,30 @@ fetch("https://jsonplaceholder.typicode.com/users/1")
     console.error("Fetch error:", error);
 
 });
+
+// Task 12.1 Practice — Fetch Single User
+
+async function getUser(id) {
+
+    try {
+
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch user");
+        }
+
+        const data = await response.json();
+
+        console.log("Single user:", data);
+
+    } catch (error) {
+
+        console.error("Error:", error);
+
+    }
+
+}
+
+getUser(1);
+
